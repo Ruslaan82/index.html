b4f03eca36931660e29e120b871674706f76b216
Ruslaan82 <abdrakhmanovv82@gmail.com>
2022-12-03 20:10:07 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//
+// var name = "Ruslan";
+// var surname = "Abdrakhmanov";
+console.log( "мое ФИО", surname + " " + name);
+//number
+var number = 123456789;
+var age = "22";
+console.log(typeof age);
+console.log(typeof number);
+
+//boolean
+var num1 = 23;
+var num2 = 14;
+console.log(num1===num2);
+
+var lastName = prompt(message: введите свое имя);
+var surName = prompt(message: введите сво фа);
